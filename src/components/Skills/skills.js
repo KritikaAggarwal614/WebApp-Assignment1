24b45e4af365c807d4c29a5b5a978c0d23c5fc29
KritikaAggarwal614 <167722586+KritikaAggarwal614@users.noreply.github.com>
2024-07-12 16:15:08 -0400

    first commit

---
+++ src/components/Skills/skills.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png';
+
+const Skills = () => {
+    return (
+        <section id='skills'>
+            <span className="skillTitle">What I do</span>
+            <span className="skillDesc">I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, and JavaScript, as well as design software such as Adobe Photoshop and Illustrator.</span>
+            <div className="skillBars">
+                <div className="skillBar">
+                    <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>UI/UX Design</h2>
+                        <p>Proficient in UI/UX design with a focus on creating intuitive and visually appealing user experiences.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>Website Design</h2>
+                        <p>Skilled in website design, creating engaging and functional digital experiences tailored to user needs and business objectives.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>App Design</h2>
+                        <p>Experienced in app design, crafting user-centered interfaces that blend functionality with seamless navigation and aesthetic appeal.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Skills;
